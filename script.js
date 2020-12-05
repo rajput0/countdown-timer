13c46892d8995cf59cb5a825450a53bbc22574a6
Raj Rajput <rajput0@sheridancollege.ca>
2020-12-05 00:07:05 -0500

    random image generation

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function updateScreen(){
     let days = Math.floor((christmas - today)/1000/60/60/24)
     document.getElementById('days').innerHTML = Math.floor(days)
     document.getElementById('hours').innerHTML = Math.floor(hours)
-    document.getElementById('minutes').innerHTML =Math.floor( mins)
+    document.getElementById('minutes').innerHTML =Math.floor(mins)
     document.getElementById('seconds').innerHTML = Math.floor(seconds)
     console.log(days, hours, mins, seconds)
 }
@@ -29,16 +29,17 @@ function createSnowFlake(){
     snow_flake.classList.add('fas');
     snow_flake.classList.add('fa-snowflake');
     snow_flake.style.left = Math.random() * (window.innerWidth) + 'px';
-    let size = (Math.random() * 7 + 7)
+
+    let size = (Math.random() * 19 + 7)
     snow_flake.style.fontSize = size + 'px'
     snow_flake.style.animationDuration = (Math.random() * size + 3) + 's';
-    snow_flake.style.opacity = (Math.random() * size) / 8;
+    snow_flake.style.opacity = (Math.random() * size) / 10;
     
     document.body.appendChild(snow_flake)
-    setTimeout(()=>{snow_flake.remove()}, 7900)
+    setTimeout(()=>{snow_flake.remove()}, 8000)
 }
 
-setInterval(createSnowFlake,80)
+setInterval(createSnowFlake,100)
 
 //console.log(`Hours ${hours} Mins ${mins} Seconds ${seconds} Days ${Math.floor(days)}`)
 
